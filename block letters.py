edf320fd1133c4a0f20c2a1345bdb9b0714c9c9e
noelelatai <73852138+noelelatai@users.noreply.github.com>
2022-06-19 02:07:51 -0400

    Create block letters.py

---
+++ block letters.py
@@ -0,0 +1,8 @@
+# NOEL EDUARDO RODRIGUEZ OBISPO se esta divirtiendo y aprendiendo con python3
+print("N   N   OOO")
+print("NN  N  O   O")
+print("N N N  O   O")
+print("N  NN  O   O")
+print("N   N  O   O")
+print("N   N  O   O")
+print("N   N   OOO")
